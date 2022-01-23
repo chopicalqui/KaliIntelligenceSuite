fa91e5ed8296fa418bf587edca591345cfe63ef5
Chopicalqui <chopicalqui@protonmail.com>
2022-01-23 20:16:32 +0100

    Merge docker-compose files

--- kis/kiscollect.py
+++ kis/kiscollect.py
@@ -40,7 +40,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-__version__ = 0.1
+__version__ = "0.3.0"
 
 import os
 import sys
